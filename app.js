1b45c23da1396190a7a07363956919a7806f8874
Uladzimir Zubovich <Uladzimir_Zubovich@epam.com>
2021-05-18 17:17:43 +0300

    fix: add port to app

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'));
 
 app.use('/api/game', game);
-app.listen(() => {
+app.listen(4000, () => {
   console.log('App is listening on 4000');
 });
